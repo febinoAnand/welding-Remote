1b844f94f91b2791767f23aca47f5a24648baf9b
aswini <aswiniprabhakaran24@gmail.com>
2024-06-27 12:45:23 +0530

    python code nd html code

---
+++ style/welding.js
@@ -0,0 +1,11 @@
+function displayData() {
+    
+    var name = document.getElementById("name").value;
+    var mod = document.getElementById("mod").value;
+    var ide = document.getElementById("ide").value;
+	
+	document.getElementById("name").value = name;
+    document.getElementById("mod").value = mod;
+    document.getElementById("ide").value = ide;
+	
+}
